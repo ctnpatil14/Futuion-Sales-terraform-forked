9d6dcd808a2d720fef6c6df25a02fbd412855e88
Sarbajit Das <65843678+sarbajitD-24@users.noreply.github.com>
2023-11-01 21:47:23 +0530

    Update ec2.tf
    updated ami id & tag

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "web"{
-  ami           = "ami-0f5ee92e2d63afc18"
+  ami           = "ami-0a7cf821b91bcccbc"
   instance_type = "t2.micro"
 
   tags = {
-    Name = "ec2"
+    Name = "ec2-ubuntu"
   }
   user_data = file("ubuntu.sh")
 }
